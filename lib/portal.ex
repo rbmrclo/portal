d3dc480ad663a079001e5525e914ad672afa3d46
Robbie Marcelo <rbmrclo@hotmail.com>
2017-01-12 14:22:08 +0800

    Portal shooting

--- lib/portal.ex
+++ lib/portal.ex
@@ -3,16 +3,47 @@ defmodule Portal do
   Documentation for Portal.
   """
 
+  defstruct [:left, :right]
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Portal.hello
-      :world
-
+  Starts transfering `data` from `left` to `right`.
   """
-  def hello do
-    :world
+  def transfer(left, right, data) do
+    for item <- data do
+      Portal.Door.push(left, item)
+    end
+
+    %Portal{left: left, right: right}
+  end
+
+  @doc """
+  Pushes data to the right in the given `portal`.
+  """
+  def push_right(portal) do
+    case Portal.Door.pop(portal.left) do
+      :error -> :ok
+      {:ok, h} -> Portal.Door.push(portal.right, h)
+    end
+
+    portal
+  end
+end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door  = inspect(left)
+    right_door = inspect(right)
+
+    left_data  = inspect(Enum.reverse(Portal.Door.get(left)))
+    right_data = inspect(Portal.Door.get(right))
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
   end
 end
